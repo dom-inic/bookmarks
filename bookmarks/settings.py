678af08752c137b169b93c7250aa4549f9d5b0b7
dom-inic <dominicnyambane22@gmail.com>
2023-07-29 17:25:36 +0300

    storing image views and ranking using redis

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -153,3 +153,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u: reverse_lazy('user_detail', args=[u.username])
 }
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
